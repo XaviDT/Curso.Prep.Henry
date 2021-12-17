59d381151ee6ce60344ce201fc2c96c3e459f69f
Javier Diaz T <xavier_imation@hotmail.com>
2021-12-17 10:12:37 -0500

    Terminado5

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -139,7 +139,7 @@ function sumarLikesDeUsuario(usuario) {
   // Tu código:
   var suma = 0;
   for (var i = 0 ; i < usuario.posts.length; i++){
-      suma = suma + usuario.posts[i]. likes ;
+      suma = suma + usuario.posts[i].likes ;
   }
   return suma ;
 }
